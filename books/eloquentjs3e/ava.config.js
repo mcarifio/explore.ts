9badbcd8a17514bcf82cab27b9fe297d2718dd1f
Mike Carifio <mike@carif.io>
2019-03-05 09:28:36 -0500

    Move all the ava config options to identify tests together in one stanza. Makes it a little easier to understand.

--- books/eloquentjs3e/ava.config.js
+++ books/eloquentjs3e/ava.config.js
@@ -4,13 +4,16 @@ export default function factory() {
 	return {
 		require: ['esm'],
     verbose: true, // eventually turn this off
-    // test files to run
-    files: [ "src/modules/__tests__/**/*.js", "src/modules/__tests__/**/*.mjs", "!node_modules/" ],
-    // triggers a new test when these sources are modified
-    "sources": [ "src/modules/*.mjs", "src/hosts/**/**.{js,mjs,html}", '!dist/**/*' ],
 	  // https://github.com/avajs/ava/blob/master/docs/recipes/babel.md#disable-avas-babel-pipeline
     babel: false,
     compileEnhancements: true,
+
+    // How to find tests and how to trigger test runs.
+    // Extensions that indicate js source files. .mjs is node for es6 modules.
     extensions: ["js", "mjs"],
+    // Test files to run when not explicitly stated.
+    files: [ "src/modules/__tests__/**/*.js", "src/modules/__tests__/**/*.mjs", "!node_modules/" ],
+    // Trigger a new test run when these sources are modified.
+    sources: [ "src/modules/*.mjs", "src/hosts/**/**.{js,mjs,html}", '!dist/**/*' ],
 	};
 };

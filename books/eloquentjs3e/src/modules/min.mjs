d03953e98a8743bbb280773b0b22a5faef0a44d9
Mike Carifio <mike@carif.io>
2019-03-05 10:30:30 -0500

    Remove CommonJS comments. Rearview mirror.

--- books/eloquentjs3e/src/modules/min.mjs
+++ books/eloquentjs3e/src/modules/min.mjs
@@ -1,8 +1,6 @@
-// let jest = require('jest');
-
-
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Functions#Rest_parameters
 // export
+
 function min(...rest) {
   let result = rest[0];
   // Don't use 'in', it's very confusing.
@@ -11,11 +9,6 @@ function min(...rest) {
   }
   return result;
 }
-
-// TODO, is there a more elegant style for this? Don't want to invest time in require given `import something from 'somewhere''
-// module.exports = {
-//     min: min,
-// };
 
 // usage: import { min } from 'min.mjs';
 export { min };

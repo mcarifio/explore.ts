8a0f1726cbf2668c9fccc562d73a707d232336b0
Mike Carifio <mcarifio@ciq.com>
2024-10-10 16:24:53 -0400

    prefix is global

--- subtrees.sh
+++ subtrees.sh
@@ -5,8 +5,8 @@ declare -g _prefix="$(realpath -Lm $(dirname "$0"))/.subtrees"
 subtree.add() (
     local _url=${1:?"${FUNCNAME} expecting a url"}
     local _remote=$(basename ${_url} .git)
-    local _prefix=${2:?"${FUNCNAME} expecting a directory"}
-    local _branch=${3:-}
+    # local _prefix=${2:?"${FUNCNAME} expecting a directory"}
+    local _branch=${2:-}
     
     [[ -z "${_remote}" ]] && { echo "no remote name found for ${_url}" >&2; return 1; }
     git remote get-url ${_remote} &> /dev/null && git remote set-url ${_remote} "${_url}" || git remote add "${_remote}" "${_url}"    
@@ -15,16 +15,16 @@ subtree.add() (
 
 subtree.pull() (
     local _remote=${1:?"${FUNCNAME} expecting a remote"}
-    local _prefix=${2:?"${FUNCNAME} expecting a directory"}
-    local _branch=${3:-}
+    # local _prefix=${2:?"${FUNCNAME} expecting a directory"}
+    local _branch=${2:-}
     git subtree pull --prefix="${_prefix}" ${_remote} ${_branch} --squash
 )
 
 subtrees() (
     for _url in "$@"; do
         [[ "${_url}" =~ ([^@]+)(@(.+))? ]] || { echo "${_url} subtree skipped." >&2; break; }
-        subtree.add "${_url}" "${_prefix}" "${BASH_REMATCH[3]}"
-        subtree.pull $(basename "${_url}" .git) "${_prefix}" "${BASH_REMATCH[3]}"
+        subtree.add "${_url}" "${BASH_REMATCH[3]}"
+        subtree.pull $(basename "${_url}" .git) "${BASH_REMATCH[3]}"
     done    
 )
 

6f6d22ddc1d50b1acecbce7352ce357208fce0b4
Mike Carifio <mcarifio@ciq.com>
2024-10-10 16:44:07 -0400

    remove set -x

--- subtrees.sh
+++ subtrees.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env bash
 set -Eeuo pipefail; shopt -s nullglob
-declare -g _prefix=".subtrees"
+declare -g _prefix=".$(basename $0 .sh)"
 
 subtree.add() (
     local _url=${1:?"${FUNCNAME} expecting a url"}
     local _remote=$(basename ${_url} .git)
     local _branch=${2:-main}
     
-    set -x
     [[ -z "${_remote}" ]] && { echo "no remote name found for ${_url}" >&2; return 1; }
     git remote get-url ${_remote} &> /dev/null && git remote set-url ${_remote} "${_url}" || git remote add "${_remote}" "${_url}"    
     git subtree add --prefix="${_prefix}" ${_remote} ${_branch} --squash    
@@ -16,7 +15,7 @@ subtree.add() (
 subtree.pull() (
     local _remote=${1:?"${FUNCNAME} expecting a remote"}
     local _branch=${2:-main}
-    set -x
+
     git subtree pull --prefix="${_prefix}" ${_remote} ${_branch} --squash
 )
 
@@ -25,7 +24,8 @@ subtrees() (
         [[ "${_url}" =~ ([^@]+)(@(.+))? ]] || { echo "${_url} subtree skipped." >&2; break; }
         subtree.add "${_url}" "${BASH_REMATCH[3]}"
         subtree.pull $(basename "${_url}" .git) "${BASH_REMATCH[3]}"
-    done    
+    done
+    echo "subtrees: ${_prefix}/*" >&2
 )
 
 subtrees gh:mcarifio/pj.git

f7829ebc60b0cacefd308e2c232a0c3b65984e4b
Mike Carifio <mcarifio@ciq.com>
2024-10-11 10:14:24 -0400

    pass _prefix to subtrees()

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -2,40 +2,43 @@
 set -Eeuo pipefail; shopt -s nullglob
 
 declare -g _top="$(realpath -Lm $(dirname $0)/..)"
-declare -g _subtrees="${_top}/subtrees"
+declare -g _subtrees="subtrees"
 
 subtree.add() (
     local _url=${1:?"${FUNCNAME} expecting a url"}
     local _remote=$(basename ${_url} .git)
     local _branch=${2:-main}
+    local _prefix=${3:-${_subtree}}
     
     [[ -z "${_remote}" ]] && { echo "no remote name found for ${_url}" >&2; return 1; }
     git remote get-url ${_remote} &> /dev/null && git remote set-url ${_remote} "${_url}" || git remote add "${_remote}" "${_url}"    
-    git subtree add --prefix="${_subtrees}/${_remote}" ${_remote} ${_branch} --squash    
+    git subtree add --prefix="${_prefix}/${_remote}" ${_remote} ${_branch} --squash    
 )
 
 subtree.pull() (
     local _remote=${1:?"${FUNCNAME} expecting a remote"}
     local _branch=${2:-main}
+    local _prefix=${3:-${_subtree}}
 
-    git subtree pull --prefix="${_subtrees}/${_remote}" ${_remote} ${_branch} --squash
+    git subtree pull --prefix="${_prefix}/${_remote}" ${_remote} ${_branch} --squash
 )
 
 subtrees() (
+    local _prefix="${1:-${_subtree}}"; shift
     for _url in "$@"; do
         [[ "${_url}" =~ ([^@]+)(@(.+))? ]] || { echo "${_url} subtree skipped." >&2; break; }
-        subtree.add "${_url}" "${BASH_REMATCH[3]}"
-        subtree.pull $(basename "${_url}" .git) "${BASH_REMATCH[3]}"
+        subtree.add "${_url}" "${BASH_REMATCH[3]}" ${_prefix}
+        subtree.pull $(basename "${_url}" .git) "${BASH_REMATCH[3]}" ${_prefix}
     done
-    printf '\n\nsubtrees created: '; find ${_subtrees} -mindepth 1 -maxdepth 1 -type d
 )
 
 main() (
-    subtrees "${@:-}"
+    local _prefix="${1:-${_subtree}}"; shift
+    subtrees "${_prefix}" "${@:-}"
     type -p direnv &> /dev/null || return 0
-    ln -sr ${_subtrees}/pj/bin/.envrc "${_top}/.envrc"
+    ln -sr ${_prefix}/pj/bin/.envrc "${_top}/.envrc"
     direnv allow
 )
 
-main gh:mcarifio/pj.git
+main ${_subtree} gh:mcarifio/pj.git
 
